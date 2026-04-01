319bb4fc980bfd08f42fcd65e8b808923fb91bd9
Broody-boy <arshdeep2875@gmail.com>
2026-04-01 19:43:32 +0530

    feat|be: type, array for attendees permissions

--- backend/src/constants/index.ts
+++ backend/src/constants/index.ts
@@ -1,2 +1,6 @@
+import { MeetingAttendeesPermissions } from "../enums";
+
 export const ALLOWED_PROFILE_PIC_MIMETYPES = ["image/jpeg", "image/png"];
 export const MAX_PERMISSIBLE_PROFILE_PIC_SIZE = 5 * 1024 * 1024; // 5mb limit
+
+export const MEETING_ATTENDEES_PERMISSIONS = Object.values(MeetingAttendeesPermissions) as readonly string[];
---
+++ backend/src/enums/index.ts
@@ -0,0 +1,6 @@
+export enum MeetingAttendeesPermissions {
+  MIC_UNMUTE = "attendees:mic-unmute", // role: subject-action
+  VIDEO_START = "attendees:video-start",
+  SCREEN_SHARE = "attendees:screen-share",
+  CHAT_MESSAGE_SEND = "attendees:chat_message-send",
+}
